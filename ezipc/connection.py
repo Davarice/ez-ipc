c5598510ffc03eae88927c1e14c9ee17a5d428a9
Davarice <jsdreim@gmail.com>
2019-05-27 19:07:57 -0400

    Implement Connection class to help with Stream encryption

--- ezipc/connection.py
+++ ezipc/connection.py
@@ -2,10 +2,11 @@ import asyncio
 
 try:
     import nacl.utils
-    from nacl.public import Box, PrivateKey
+    from nacl.public import Box, PrivateKey, PublicKey
 except ImportError:
     Box = None
     PrivateKey = None
+    PublicKey = None
     can_encrypt = False
 else:
     can_encrypt = True
@@ -16,27 +17,48 @@ class Connection:
         self.instr: asyncio.StreamReader = instr
         self.outstr: asyncio.StreamWriter = outstr
 
-        self.key = PrivateKey.generate() if can_encrypt else None
+        self._key = PrivateKey.generate() if can_encrypt else None
         self.key_other = None
+        self._box = None
         self.box = None
 
-    def add_key(self, pkey):
-        pass
+    @property
+    def key(self):
+        return self._key.public_key if can_encrypt else None
 
-    def decrypt(self, data: bytes):
-        pass
+    def add_key(self, pubkey: bytes):
+        if can_encrypt:
+            self.key_other = PublicKey(pubkey)
+            self._box = Box(self.key, self.key_other)
 
-    def encrypt(self, data: bytes):
-        pass
+    def begin_encryption(self):
+        self.box = self._box
+
+    def decrypt(self, data):
+        if self.box:
+            return self.box.decrypt(data)
+        else:
+            return data
+
+    def encrypt(self, data):
+        if self.box:
+            return self.box.encrypt(data)
+        else:
+            return data
 
     async def readline(self):
-        pass
+        data = self.instr.readline()
+        return self.decrypt(data)
 
-    async def readuntil(self, separator=b"\n"):
-        pass
+    async def readuntil(self, *a, **kw):
+        data = self.instr.readuntil(*a, **kw)
+        return self.decrypt(data)
 
-    async def read(self, n=-1):
-        pass
+    async def read(self, *a, **kw):
+        data = self.instr.read(*a, **kw)
+        return self.decrypt(data)
 
-    async def write(self, data):
-        pass
+    async def write(self, data: bytes):
+        self.outstr.write(self.encrypt(data))
+        self.outstr.write(b"\n")
+        await self.outstr.drain()
