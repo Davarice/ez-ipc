130c1bd2234e89873e3ccf709de2440b92285656
Davarice <jsdreim@gmail.com>
2019-05-31 19:34:28 -0400

    Use `functools.wraps()` in place of low quality custom decorator updater

--- ezipc/util/callbacks.py
+++ ezipc/util/callbacks.py
@@ -1,26 +1,9 @@
 """Module defining Callback functions to be executed with Futures."""
 
 import asyncio
+from functools import wraps
 
 
-def _deco(decorator):
-    """Wrap a Decorator so that the Function it returns is given the same Name
-        and Docstring as the Function it was given.
-
-    Writing this gave me a headache. Worth it? Probably not. But whatever.
-    """
-    def decorated(function):
-        callback = decorator(function)
-
-        callback.__doc__ = function.__doc__
-        callback.__name__ = function.__name__
-
-        return callback
-
-    return decorated
-
-
-@_deco
 def callback_response(func):
     """Prepare a given Function to interpret a Future as its Result instead, so
         that the Function does not need to worry about Exceptions.
@@ -29,6 +12,7 @@ def callback_response(func):
         by any Callback that uses this Decorator.
     """
 
+    @wraps(func)
     def callback(future: asyncio.Future, remote=None):
         if future.done() and not (future.cancelled() or future.exception()):
             return func(future.result(), remote=remote)
