dc4ee43040fb265851d4bbb57c4412c822f622cb
Davarice <jsdreim@gmail.com>
2019-08-05 17:48:49 -0400

    Migrate from Colorama to Blessings

--- ezipc/util/output.py
+++ ezipc/util/output.py
@@ -1,58 +1,44 @@
 """Module defining functions for printing to the Console."""
 
 from datetime import datetime as dt
-from typing import List, Union
+from typing import Callable, Dict, List, Tuple, Union
+
+
+NOCOLOR = lambda s: s
 
 
 class Fake:
-    BLACK = ""
-    RED = ""
-    GREEN = ""
-    YELLOW = ""
-    BLUE = ""
-    MAGENTA = ""
-    CYAN = ""
-    WHITE = ""
-    RESET = ""
-    LIGHTBLACK_EX = ""
-    LIGHTRED_EX = ""
-    LIGHTGREEN_EX = ""
-    LIGHTYELLOW_EX = ""
-    LIGHTBLUE_EX = ""
-    LIGHTMAGENTA_EX = ""
-    LIGHTCYAN_EX = ""
-    LIGHTWHITE_EX = ""
+    def __getattr__(self, attr):
+        return NOCOLOR
 
 
 try:
-    from colorama import init, Fore
-
-    init()
+    from blessings import Terminal
 except ImportError:
-    init = lambda: None
-    Fore = Fake
+    Terminal = Fake
 
-Color = Fore
-prefices = {}
+T = Terminal()
+Color = T
+colors: Dict[str, Tuple[Callable[[str], str], str, int]] = {}
 
 
 def set_colors(use_real: bool):
     global Color
-    global prefices
+    global colors
 
-    Color = Fore if use_real else Fake
-    prefices = {
-        "": (Color.WHITE, "", 1),
-        "con": (Color.WHITE, " ++", 1),
-        "dcon": (Color.LIGHTBLACK_EX, "X- ", 1),
-        "win": (Color.LIGHTGREEN_EX, "\o/", 1),
-        "diff": (Color.WHITE, "*- ", 2),
-        "err": (Color.MAGENTA, "x!x", 1),
-        "recv": (Color.WHITE, "-->", 3),
-        "send": (Color.LIGHTBLACK_EX, "<--", 3),
-        "tab": (Color.WHITE, "   ", 3),
-        "warn": (Color.MAGENTA, "(!)", 3),
-        "info": (Color.CYAN, "(!)", 2),
+    Color = T if use_real else Fake()
+    colors = {
+        "": (Color.white, "", 1),
+        "con": (Color.white, " ++", 1),
+        "dcon": (Color.bright_black, "X- ", 1),
+        "win": (Color.bright_green, "\o/", 1),
+        "diff": (Color.white, "*- ", 2),
+        "err": (Color.magenta, "x!x", 1),
+        "recv": (Color.white, "-->", 3),
+        "send": (Color.bright_black, "<--", 3),
+        "tab": (Color.white, "   ", 3),
+        "warn": (Color.magenta, "(!)", 3),
+        "info": (Color.cyan, "(!)", 2),
     }
 
 
@@ -66,9 +52,9 @@ class _Printer:
         self.startup: dt = dt.utcnow()
         self.verbosity: int = verbosity
 
-    def emit(self, etype: str, text: str, color: str = ""):
+    def emit(self, etype: str, text: str, color=None):
         now = dt.utcnow()
-        p_color, prefix, pri = prefices.get(etype) or (Color.WHITE, etype, 4)
+        p_color, prefix, pri = colors.get(etype) or (Color.white, etype, 4)
         if self.file:
             print(
                 "<{}> {} {}".format(now.isoformat(sep=" ")[:-3], prefix, str(text)),
@@ -82,8 +68,8 @@ class _Printer:
                     # "<{} | {}> {} {}".format(  # Two Times format
                     str(now)[11:-4],  # Current Time
                     # str(now - self.startup)[:-7],  # Server Uptime
-                    p_color + prefix,
-                    (color or Color.RESET) + str(text) + Color.RESET,
+                    p_color(prefix),
+                    (color or NOCOLOR)(str(text)),
                 )
             )
 
@@ -100,13 +86,13 @@ def echo(etype: str, text: Union[str, List[str]], color=""):
 
 
 def err(text: str, exc: BaseException = None):
-    if exc:
+    if exc is not None:
         text += " {} - {}".format(type(exc).__name__, exc)
-    echo("err", text, Color.RED)
+    echo("err", text, Color.red)
 
 
 def warn(text: str):
-    echo("warn", text, Color.LIGHTYELLOW_EX)
+    echo("warn", text, Color.bright_yellow)
 
 
 def set_verbosity(n: int):
